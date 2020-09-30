4f0b8201de2b411ea79ed58525b183693bdabb4d
Renato <neo_rlm@hotmail.com>
2020-09-30 17:17:56 -0300

    actionPageGrupo

--- src/pages/Grupos/index.js
+++ src/pages/Grupos/index.js
@@ -1,33 +1,87 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from "react";
 
-import './styles.css';
-import InputLabel from '../../components/InputLabel';
-import InputForm from '../../components/InputForm';
+import "./styles.css";
+import InputLabel from "../../components/InputLabel";
+import InputForm from "../../components/InputForm";
 
 function Grupos() {
-  const [newGroup, setNewGroup] = useState(true);
+  const [newGroup, setNewGroup] = useState(false);
+  const [groupEdit, setGroupEdit] = useState(null);
+  const [grupos, setGrupos] = useState([
+    { _id: 1, name: "Admin" },
+    { _id: 2, name: "User" },
+  ]);
+  const groupToEdit = (e) => {
+    setGroupEdit(e);
+    setNewGroup(true);
+  };
+  const handleNewGroup = () => setNewGroup(!newGroup);
+  const createdGroup = (e) => {
+    setGrupos([...grupos, e]);
+    handleNewGroup();
+  };
+  const handleDeletGroup = (e) => {
+    const filterGroup = grupos.filter((g) => g._id !== e);
+    setGrupos(filterGroup);
+  };
   return (
     <div className="mainGrupos">
       {!newGroup && (
-        <ListGroups>
-          <button type="submit">Novo Grupo</button>
+        <ListGroups
+          grupos={grupos}
+          editGroup={groupToEdit}
+          deleteGrupo={handleDeletGroup}
+        >
+          <button type="submit" onClick={handleNewGroup} className="button ">
+            Novo Grupo
+          </button>
         </ListGroups>
       )}
-      {newGroup && <FormGrupos />}
+      {newGroup && (
+        <FormGrupos
+          cancel={handleNewGroup}
+          newGroup={createdGroup}
+          editGroup={groupEdit}
+        />
+      )}
     </div>
   );
 }
-const ListGroups = ({ children }) => {
+const ListGroups = ({ grupos, children, editGroup, deleteGrupo }) => {
+  const groupForEdit = (e) => {
+    editGroup(e);
+  };
+  const apagarGrupo = (e) => {
+    deleteGrupo(e);
+  };
   return (
     <div className="listOfGroups">
-      Grupos
+      <h2>Grupos</h2>
       {children}
-      <ul>list Groups</ul>
+      <ul>
+        {grupos.map((grupo) => (
+          <li key={grupo._id}>
+            <span className="descGrupo">{grupo.name}</span>
+            <div className="groupBtn">
+              <button className="button" onClick={() => groupForEdit(grupo)}>
+                Editar
+              </button>
+              <button
+                className="button button-danger"
+                onClick={() => apagarGrupo(grupo._id)}
+              >
+                {" "}
+                Apagar
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
-const FormGrupos = () => {
-  const [name, setName] = useState('');
+const FormGrupos = ({ cancel, newGroup, editGroup }) => {
+  const [name, setName] = useState("");
   const handleSetName = (e) => {
     setName(e.target.value);
   };
@@ -36,8 +90,16 @@ const FormGrupos = () => {
     const data = {
       name,
     };
-    console.log(data);
+    newGroup(data);
   };
+  const dateGroupEdit = useCallback(() => {
+    if (editGroup) {
+      setName(editGroup.name);
+    }
+  }, []);
+  useEffect(() => {
+    dateGroupEdit();
+  }, []);
   return (
     <div className="forms">
       <h2>Cadastro novo grupo</h2>
@@ -52,7 +114,11 @@ const FormGrupos = () => {
           <button type="submit" className="button">
             Gravar
           </button>
-          <button type="submit" className="button button-danger">
+          <button
+            type="submit"
+            className="button button-danger"
+            onClick={cancel}
+          >
             Cancelar
           </button>
         </div>
