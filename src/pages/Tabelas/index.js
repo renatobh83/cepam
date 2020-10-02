50751c18e4d3438d1a797ee56be839dd8385dc8c
Renatobh83 <neo_rlm@hotmail.com>
2020-10-01 22:28:34 -0300

    tabelasInserirExames

--- src/pages/Tabelas/index.js
+++ src/pages/Tabelas/index.js
@@ -2,6 +2,8 @@ import React, { Children, useCallback, useEffect, useState } from 'react';
 import InputForm from '../../components/InputForm';
 
 import './styles.css';
+import InputMask from 'react-input-mask';
+import { FiTrash2 } from 'react-icons/fi';
 
 function Tabelas() {
   const [novaTabela, setNovaTabela] = useState(false);
@@ -83,7 +85,90 @@ const ListTabelas = ({ children, tabelas }) => {
 
 const InserirExame = ({ tabela }) => {
   console.log(tabela[0]);
-  return <>Ls</>;
+  const [exames, setExames] = useState([
+    { _id: 1, name: 'Rx Torax' },
+    { _id: 2, name: 'Rx Torax pa' },
+  ]);
+  const [examesSelecionados, setExamesSelecionados] = useState([]);
+  const insertExame = (e, x) => {
+    if (x.target.checked) {
+      setExamesSelecionados([...examesSelecionados, { exame: e, valor: null }]);
+      const filterES = exames.filter((exame) => exame.name !== e.name);
+      setExames(filterES);
+    } else {
+      deleteExame(e);
+    }
+  };
+  const deleteExame = (e) => {
+    const newArray = examesSelecionados.filter(
+      (ex) => ex.exame.name !== e.exame.name
+    );
+    setExames([...exames, e.exame]);
+    setExamesSelecionados(newArray);
+  };
+
+  return (
+    <div className="examesList">
+      <ul>
+        {exames.map((exame) => (
+          <li key={exame._id}>
+            <label htmlFor={exame._id}>
+              <input
+                type="checkbox"
+                id={exame._id}
+                name="exames"
+                className="exames-check"
+                defaultValue={exame._id}
+                onClick={(e) => insertExame(exame, e)}
+              />
+              {exame.name}
+            </label>
+          </li>
+        ))}
+      </ul>
+      {examesSelecionados.length > 0 && (
+        <div className="examesSelecionados">
+          {examesSelecionados.map((exame, index) => (
+            <div className="examesSelect" key={index}>
+              <div className="delete">
+                <button>
+                  <FiTrash2
+                    size={15}
+                    color={'red'}
+                    onClick={() => deleteExame(exame)}
+                  />
+                </button>
+              </div>
+              <div className="contentExamInTable">
+                <span> {exame.exame.name}</span>
+                <InputMask
+                  type="text"
+                  id="valor"
+                  inputmode="decimal"
+                  defaultValue={examesSelecionados[index].valor}
+                  placeholder="Valor"
+                  // onChange={(e) => handleChangeInput(e, index)}
+                  // onKeyPress={(e) => moeda(e.target.value)}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+      <div className="inputGroup">
+        <button type="submit" className="button" onClick={() => {}}>
+          Gravar
+        </button>
+        <button
+          type="submit"
+          className="button button-danger"
+          onClick={() => {}}
+        >
+          Cancelar
+        </button>
+      </div>
+    </div>
+  );
 };
 const FormTabela = ({ cancel, create }) => {
   const [name, setName] = useState('');
