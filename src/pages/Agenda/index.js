a847a0b183586f36452b40e42d5a0458eb885cdf
Renato mendonca <neo_rlm@hotmail.com>
2020-10-31 11:28:34 -0300

    ajustAgendaView

--- src/pages/Agenda/index.js
+++ src/pages/Agenda/index.js
@@ -18,30 +18,26 @@ export default function Agenda() {
   };
 
   const onChangeSala = (e) => {
+    let dataAgenda = [];
+    setHorarios([]);
+    setData(dataAgenda);
     if (e === '#') {
       setData([]);
     } else {
-      setHorarios([]);
-      let dataAgenda = [];
-      // const agendamentoSala = agendamentos.filter((s) => s.sala._id === e);
-      // agendamentoSala.forEach((d) => {
-      //   if (!dataAgenda.includes(d.dados.horario.data)) {
-      //     dataAgenda.push(d.dados.horario.data);
-      //   } else {
-      //   }
-      // });
-      // setData(dataAgenda);
-      for (let i = 0; i < 30; i++) {
-        dataAgenda.push(i);
-      }
+      const agendamentoSala = agendamentos.filter((s) => s.sala._id === e);
+      agendamentoSala.forEach((d) => {
+        if (!dataAgenda.includes(d.dados.horario.data)) {
+          dataAgenda.push(d.dados.horario.data);
+        }
+      });
+
       setData(dataAgenda);
-      // setAgendaSala(agendamentoSala);
+      setAgendaSala(agendamentoSala);
     }
   };
   const chooseData = (e) => {
     const horarios = agendaSala.filter((data) => data.dados.horario.data === e);
     setHorarios(horarios);
-    console.log(horarios);
   };
 
   useEffect(() => {
@@ -64,29 +60,25 @@ export default function Agenda() {
       </select>
       <div className="agenda">
         <ul>
-          <>
-            <FiArrowLeft />
-            {data.map((periodo) => (
-              <li key={periodo}>
-                <label htmlFor={periodo}>
-                  <div className="cardHorario">{periodo}</div>
-                  <input
-                    type="radio"
-                    name="horario"
-                    id={periodo}
-                    onChange={() => chooseData(periodo)}
-                  />
-                </label>
-              </li>
-            ))}
-            <FiArrowRight />
-          </>
+          {data.map((periodo, index) => (
+            <li key={periodo}>
+              <label htmlFor={index}>
+                <input
+                  type="radio"
+                  name="horario"
+                  id={index}
+                  onChange={() => chooseData(periodo)}
+                />
+                <div className="cardHorario">{periodo}</div>
+              </label>
+            </li>
+          ))}
         </ul>
         <ul id="horarios">
           {horarios.map((horario) => (
-            <li>
+            <li key={horario.dados.horario.horaInicio}>
               {horario.dados.horario.data} - {horario.dados.horario.horaInicio}{' '}
-              -{horario.dados.exame.name} - {horario.paciente.name}
+              - {horario.dados.exame.name} - {horario.paciente.name}
             </li>
           ))}
         </ul>
